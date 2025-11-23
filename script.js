20d7c4970252c79c432a5d5484984aef70013efd
anm30222uca06 <148445828+anm30222uca06@users.noreply.github.com>
2025-11-23 00:08:51 -1200

    new project

---
+++ script.js
@@ -0,0 +1,14 @@
+var sidenav= document.querySelector(".side-navbar")
+ 
+
+function showNavbar()
+{
+    
+    sidenav.style.display = "block"
+}
+
+function cancelbar()
+{
+    
+    sidenav.style.display = "none"
+}
